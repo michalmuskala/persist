80369a7063caa662225dd8646552634411e0eb14
Michal Muskala <michal@muskala.eu>
2016-07-21 00:35:05 +0200

    Add gen_stage

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Persist.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :gen_stage]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,6 @@ defmodule Persist.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:gen_stage, "~> 0.4"}]
   end
 end
